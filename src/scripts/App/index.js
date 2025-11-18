9bb9562f3caa1f3963df121a4f1c9b82ebdbf153
Carlos Tremonte de Lemos <carlos.tlemos@gmail.com>
2025-11-18 18:12:04 -0300

    adição de seção por clique

--- src/scripts/App/index.js
+++ src/scripts/App/index.js
@@ -6,22 +6,46 @@ export default class App {
     this.container = document.getElementById('main');
   }
 
+  pickNext() {
+    const current = this.data[this.finalList[this.navPosition]];
+    const sortPot = [...current.relations]
+      .filter(
+        (item) => !this.finalList.includes(item.target) // eslint-disable-line
+      )
+      .slice(0, 5);
+
+    const pickIndex = Math.floor((sortPot.length - 1) * Math.random());
+    const { target } = sortPot[pickIndex];
+    this.container.appendChild(this.data[target].DOMElement);
+    this.finalList.push(target);
+    localStorage.setItem('finalList', JSON.stringify(this.finalList));
+
+    console.log('adicionou', this.data[target]);
+  }
+
   bindNav() {
-    const navDown = document.getElementById('up');
-    const navUp = document.getElementById('down');
+    const navPrevious = document.getElementById('up');
+    const navNext = document.getElementById('down');
     const { height } = this.container.getBoundingClientRect();
 
     this.navPosition = !localStorage.getItem('navPosition')
       ? 0
       : Number(localStorage.getItem('navPosition'));
 
+    localStorage.setItem('navPosition', this.navPosition);
+
     this.container.scrollTo({
       top: height * this.navPosition
     });
 
-    navUp.addEventListener('click', () => {
+    navNext.addEventListener('click', () => {
       if (this.navPosition < this.data.length) {
+        if (!this.finalList[this.navPosition + 1]) {
+          this.pickNext();
+        }
+
         this.navPosition += 1;
+
         this.container.scrollTo({
           top: height * this.navPosition,
           behavior: 'smooth'
@@ -30,7 +54,7 @@ export default class App {
       }
     });
 
-    navDown.addEventListener('click', () => {
+    navPrevious.addEventListener('click', () => {
       if (this.navPosition > 0) {
         this.navPosition -= 1;
         this.container.scrollTo({
@@ -43,6 +67,34 @@ export default class App {
   }
 
   handleData() {
+    this.data.forEach((entry) => {
+      const item = entry;
+      const { text, autor, obra, topicos, relations } = item;
+      item.DOMElement = document.createElement('section');
+
+      item.DOMElement.innerHTML = renderNote(text, autor, obra, topicos);
+      item.DOMElement.classList.add('dayentry');
+
+      relations.sort((a, b) => b.weight - a.weight);
+    });
+
+    if (!localStorage.getItem('finalList')) {
+      const pickIndex = Math.floor((this.data.length - 1) * Math.random());
+      this.finalList = [];
+      this.finalList.push(pickIndex);
+    } else {
+      this.finalList = JSON.parse(localStorage.getItem('finalList'));
+    }
+
+    [...this.finalList]
+      .map((key) => this.data[key])
+      .forEach((item) => {
+        this.container.appendChild(item.DOMElement);
+      });
+
+    localStorage.setItem('finalList', JSON.stringify(this.finalList));
+
+    /*
     // random picks Array
     if (!localStorage.getItem('finalList')) {
       this.finalList = [];
@@ -77,6 +129,7 @@ export default class App {
 
         this.container.appendChild(item.DOMElement);
       });
+    */
   }
 
   start() {
