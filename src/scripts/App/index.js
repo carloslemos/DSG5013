d9619a3d133efa9de8c9ad4e9a713830c4415758
Carlos Tremonte de Lemos <carlos.tlemos@gmail.com>
2025-11-18 20:01:36 -0300

    corrigindo problema no final da navegação

--- src/scripts/App/index.js
+++ src/scripts/App/index.js
@@ -39,7 +39,7 @@ export default class App {
     });
 
     navNext.addEventListener('click', () => {
-      if (this.navPosition < this.data.length) {
+      if (this.navPosition < this.data.length - 1) {
         if (!this.finalList[this.navPosition + 1]) {
           this.pickNext();
         }
@@ -93,43 +93,6 @@ export default class App {
       });
 
     localStorage.setItem('finalList', JSON.stringify(this.finalList));
-
-    /*
-    // random picks Array
-    if (!localStorage.getItem('finalList')) {
-      this.finalList = [];
-      let pickIndex = Math.round(this.data.length * Math.random());
-      this.finalList.push(pickIndex);
-
-      while (this.finalList.length < this.data.length) {
-        const pickElement = this.data[pickIndex];
-        const related = [...pickElement.relations].filter(
-          (item) => ![...this.finalList].includes(item.target) // eslint-disable-line
-        );
-        related.sort((a, b) => b.weight - a.weight);
-        pickIndex = Number(related[0].target);
-        this.finalList.push(pickIndex);
-      }
-
-      localStorage.setItem('finalList', JSON.stringify(this.finalList));
-    } else {
-      this.finalList = JSON.parse(localStorage.getItem('finalList'));
-    }
-
-    // prints Data
-    [...this.finalList]
-      .map((key) => this.data[key])
-      .forEach((entry) => {
-        const item = entry;
-        const { text, autor, obra, topicos } = item;
-        item.DOMElement = document.createElement('section');
-
-        item.DOMElement.innerHTML = renderNote(text, autor, obra, topicos);
-        item.DOMElement.classList.add('dayentry');
-
-        this.container.appendChild(item.DOMElement);
-      });
-    */
   }
 
   start() {
